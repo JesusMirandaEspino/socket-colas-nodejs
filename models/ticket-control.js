d0195e33288041daf8c8e48f87fd402c81bcc951
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-24 22:30:44 -0500

    model ticket

---
+++ models/ticket-control.js
@@ -0,0 +1,11 @@
+class TicketControl {
+
+
+    constructor(){
+        this.ultimo   = 0;
+        this.hoy      = new Date().getDate();
+        this.ticket   = [];
+        this.ultimos4 = [];
+    }
+
+}
